30b05046ef74765da804a9915b2b9ba76ea9868a
X140Yu <zhaoxinyu1994@gmail.com>
2017-02-02 14:53:50 +0800

    Initial commit

---
+++ DayCache/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DayCache
+//
+//  Created by X140Yu on 2/2/17.
+//  Copyright © 2017 X140Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
